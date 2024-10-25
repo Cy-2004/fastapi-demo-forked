c9db97e7d1d57c7914ea5b3c033429be460310c8
Cy-2004 <cy2004@Chelsea>
2024-10-25 03:27:07 -0400

    updated build.yaml and main.py

--- app/main.py
+++ app/main.py
@@ -5,13 +5,22 @@ from typing import Optional
 from pydantic import BaseModel
 import json
 import os
+import math
 
 app = FastAPI()
 
 @app.get("/")  # zone apex
 def zone_apex():
-    return {"Hello": "Hello API"}
+    return {"Good day": "Sunshine!"}
 
 @app.get("/add/{a}/{b}")
 def add(a: int, b: int):
     return {"sum": a + b}
+
+@app.get("/multiply/{c}/{d}")
+def multiply(c: int, d: int):
+    return {"product": c * d}
+
+@app.get("/square/{e}")
+def square(e: int):
+    return {"square": math.sqrt(e)}
